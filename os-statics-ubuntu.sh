9837c0fc778b51d030a3f3a4911d6f0668c51ce5
jackorn ( JIGAR SONI ) <51534139+jigarsoni17@users.noreply.github.com>
2022-09-12 22:21:42 +0530

    Update os-statics-ubuntu.sh

--- os-statics-ubuntu.sh
+++ os-statics-ubuntu.sh
@@ -5,11 +5,9 @@ DATE=$(date +%d%m%y)
 
 echo -e "Server host Name: `hostname`" > /tmp/osi-$DATE.txt
 echo -e "Kernel version: `uname -r`" >> /tmp/osi-$DATE.txt
-echo -e "Mounted & Usage : `df -h / | tail -1 | awk '{print $6, $3}'`" >> /tmp/osi-$DATE.txt
+echo -e "total-Mounted-Usage : `df -h / | tail -1 | awk awk '{print $2, $3, $6}'`" >> /tmp/osi-$DATE.txt
 echo -e "/swap partition Usage: `free -h | tail -1 | awk '{print $3}'`" >> /tmp/osi-$DATE.txt
-#echo -e "/home partition size"
 echo -e "load average: `uptime | grep 'load average:'| tail -c 17 `" >> /tmp/osi-$DATE.txt
-#echo -e "Lowest load avg: `uptime | grep 'load average:'| tail -c 17 | awk '{print $2}'| cut -f1 -d,`" >> text.txt
 echo -e "Last boot: `who -b | tail -1 | awk '{print $3, $4}'`" >> /tmp/osi-$DATE.txt
 echo -e "`cat /etc/os-release | head -5 | tail -1`" >> /tmp/osi-$DATE.txt
 sudo sed -i 's/PRETTY/OS/' /tmp/osi-$DATE.txt
