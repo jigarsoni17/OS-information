b0ff28f8f32de7add5b914cf4b7467fa687a9851
jackorn ( JIGAR SONI ) <51534139+jigarsoni17@users.noreply.github.com>
2022-09-13 10:18:23 +0530

    Update os-statics-ubuntu.sh

--- os-statics-ubuntu.sh
+++ os-statics-ubuntu.sh
@@ -6,7 +6,7 @@ DATE=$(date +%d%m%y)
 echo -e "Server host Name: `hostname`" > /tmp/osi-$DATE.txt
 echo -e "Kernel version: `uname -r`" >> /tmp/osi-$DATE.txt
 echo -e "total-Mounted-Usage : `df -h / | tail -1 | awk awk '{print $2, $3, $6}'`" >> /tmp/osi-$DATE.txt
-echo -e "/swap partition Usage: `free -h | tail -1 | awk '{print $3}'`" >> /tmp/osi-$DATE.txt
+echo -e "/swap partition total-Usage: `free -h | tail -1 | awk '{print $2, $3}'`" >> /tmp/osi-$DATE.txt
 echo -e "load average: `uptime | grep 'load average:'| tail -c 17 `" >> /tmp/osi-$DATE.txt
 echo -e "Last boot: `who -b | tail -1 | awk '{print $3, $4}'`" >> /tmp/osi-$DATE.txt
 echo -e "`cat /etc/os-release | head -5 | tail -1`" >> /tmp/osi-$DATE.txt
